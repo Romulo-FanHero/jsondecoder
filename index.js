80da22e7ccf81a0bc55636e9515a0c8b75aaecd2
Romulo-FanHero <romulo@fanhero.com>
2016-09-12 07:54:00 -0300

    some-changes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('alguma coisa');
+var fs = require('fs'),
+  JSONStream = require('JSONStream'),
+  es = require('event-stream');
+
+var getStream = function () {
+    var jsonData = 'temp.json',
+        stream = fs.createReadStream(jsonData, {encoding: 'utf8'}),
+        parser = JSONStream.parse('*');
+        return stream.pipe(parser);
+};
+
+ getStream()
+  .pipe(es.mapSync(function (data) {
+    console.log(data);
+  }));
