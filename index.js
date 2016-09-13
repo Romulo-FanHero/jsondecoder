a0b3dc8024c633e647185115f8109c184ca0c949
Romulo-FanHero <romulo@fanhero.com>
2016-09-12 21:34:53 -0300

    fixer-parser-cleaned

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 var _ = require('lodash');
 var fs = require('fs');
 
-//var originalFileData = fs.readFileSync('temp/temp.json', 'utf8');
-//var firstLineRemoved = originalFileData.split(`/* 1 */`).join(``);
-//var objCallRemoved = (firstLineRemoved.split(`ObjectId("`).join(`"`)).split(`")`).join(`"`);
-//fs.writeFileSync('temp/temp2.json', objCallRemoved);
-
-//fs.writeFileSync('temp/fixed.json',((((fs.readFileSync('temp/temp.json', 'utf8')).split(`/* 1 */`).join(``)).split(`ObjectId("`).join(`"`)).split(`")`).join(`"`)));
-
-//fs.writeFileSync('temp/fixed.json', (((
-//                fs.readFileSync('temp/temp.json', 'utf8')
-//            ).split(`/* 1 */`).join(``)
-//        ).split(`ObjectIdx"`).join(`"`)
-//    ).split(`"x`).join(`"`)
-//);
-
-fs.writeFileSync('temp/fixed.json',
-    fs.readFileSync('temp/temp.json', 'utf8')
-    .split(`/* 1 */`).join(``)
-    .split(`ObjectId("`).join(`"`)
-    .split(`")`).join(`"`)
-);
+const dumpFilePath = 'temp/temp.json';
+const fixedFilePath = 'temp/fixed.json';
 
 try {
+    // check if fixed file already exist, thowing an exception if it doesn't
+    fs.accessSync(fixedFilePath, fs.F_OK);
+}
+catch (e) {
+    // try to create fixed file
+    try {
+        fs.writeFileSync(fixedFilePath,
+            fs.readFileSync(dumpFilePath, 'utf8')
+            .split(`/* 1 */`).join(``)
+            .split(`ObjectId("`).join(`"`)
+            .split(`")`).join(`"`)
+        );
+    }
+    catch (err) {
+        console.log('error while trying to fix dump file: ', err);
+        process.exit();
+    }
+}
+
+try {
+    // try to parse fixed file
     var users = JSON.parse(fs.readFileSync('temp/fixed.json', 'utf8'));
-    var cnt = 0;
-    users.forEach(function(user) {
-        if (
-            _.has(user, 'custom.status') && user.custom.status === 'ENABLED'
-        ) {
-            cnt++;
-        }
-    });
-    console.log('first 10 fans', JSON.stringify(users.slice(0, 10), null, 2));
-    console.log('COUNT', cnt);
 }
 catch (err) {
-    console.error(err);
+    console.log('error while parsing fixed file: ', err);
+    process.exit();
 }
+
+var cnt = 0;
+users.forEach(function(user) {
+    if (_.has(user, 'custom.status')) {
+        cnt++;
+    }
+});
+console.log('First 10 fans', JSON.stringify(users.slice(0, 10), null, 2));
+console.log('COUNT', cnt);
