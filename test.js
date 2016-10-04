6c0e91d2a8250dab0186a563b21f38c8811d5224
Romulo-FanHero <romulo@fanhero.com>
2016-10-04 19:38:12 -0300

    test-fixes-without-useless-dependencies

--- test.js
+++ test.js
@@ -1,38 +1,20 @@
 require('events').EventEmitter.prototype._maxListeners = 0;
 
-var prompt = require('prompt');
-
 var _ = require('lodash');
 var sleep = require('system-sleep');
 
 var fs = require('fs'),
     readline = require('readline');
 
-//const inputFilePath = 'temp/mod_inter.json';
-const inputFilePath = 'temp/full.json';
-//const inputFilePath = 'temp/intermediate.json';
-
-var rd = readline.createInterface({
-    input: fs.createReadStream(inputFilePath),
-    output: process.stdout,
-    terminal: false
-});
-
 var buff = '';
 var users = [];
-var cnt = 0;
-rd.on('line', function(line) {
-    if (cnt === 36327) {
-        //console.log('buffer:');
-        //console.log(buff);
-        //console.log();
-        console.log('Sleeping...');
-        sleep(86400000);
-    }
+
+readline.createInterface({
+    input: fs.createReadStream('temp/full.json'),
+    output: process.stdout,
+    terminal: false
+}).on('line', function(line) {
     try {
-        //var pars = JSON.parse(buff + line.split('},').join('}'));
-        //cnt++;
-        //console.log(cnt);
         users.push(JSON.parse(buff + line.split('},').join('}')));
         console.log(users.length);
         buff = '';
